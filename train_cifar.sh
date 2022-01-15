b910043d364f20aabf6d75f58d2e508a6bff18ae
arthurhero <chenziwe@grinnell.edu>
2022-01-15 11:31:56 -0800

    added cifar model

--- train_cifar.sh
+++ train_cifar.sh
@@ -1,3 +1,3 @@
 python -m torch.distributed.launch --nproc_per_node=8 --use_env main.py --model LeViT_128S_cifar\
-    --data-path ../datasets/cifar --data-set CIFAR --input-size 32 \
+    --data-path ../datasets/cifar10 --data-set CIFAR --input-size 32 \
     --output_dir ./checkpoints/
