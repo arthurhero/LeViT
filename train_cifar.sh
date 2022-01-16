0872c711da17885ed23bae8a3150ebc120a6f254
arthurhero <chenziwe@grinnell.edu>
2022-01-15 20:17:26 -0800

    fix bug

--- train_cifar.sh
+++ train_cifar.sh
@@ -1,5 +1,5 @@
 OUT_DIR="./checkpoints/"
-CKPT_NAME="cifar_simp_aug_32.ckpt"
+CKPT_NAME="cifar10_simp_aug_32.ckpt"
 
 CUDA_VISIBLE_DEVICES=0 WORLD_SIZE=1 python -m torch.distributed.launch --nproc_per_node=1 \
     --master_port 47771 --use_env main.py --model LeViT_128S_cifar\
